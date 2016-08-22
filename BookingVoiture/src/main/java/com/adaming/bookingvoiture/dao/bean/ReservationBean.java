148e8439740c8548a6f92d59c078cf30b9b831bb
juliebrouque <julie.brouque@live.fr>
2016-08-22 11:20:01 +0200

    Bean

--- BookingVoiture/src/main/java/com/adaming/bookingvoiture/dao/bean/ReservationBean.java
+++ BookingVoiture/src/main/java/com/adaming/bookingvoiture/dao/bean/ReservationBean.java
@@ -1,16 +1,267 @@
 package com.adaming.bookingvoiture.dao.bean;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.RequestScoped;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.adaming.bookingvoiture.entities.Client;
+import com.adaming.bookingvoiture.entities.Reservation;
+import com.adaming.bookingvoiture.entities.Voiture;
+import com.adaming.bookingvoiture.exceptions.ExceptionDeleteRes;
+import com.adaming.bookingvoiture.exceptions.ExceptionResDispo;
+import com.adaming.bookingvoiture.service.client.IClientService;
 import com.adaming.bookingvoiture.service.reservation.IReservationService;
+import com.adaming.bookingvoiture.service.voiture.IVoitureService;
 
-/*@Component("ReservationBean")
+@Component("ReservationBean")
 @RequestScoped
 public class ReservationBean {
-@Autowired
+	@Autowired
 	private IReservationService serviceReservation;
 	
-}*/
+	@Autowired
+	private IClientService serviceClient;
+	@Autowired
+	private IVoitureService serviceVoiture;
+
+	// Attributs
+
+	private Long idReservation;
+	private Long idClient;
+	private Long idVoiture;
+	  private double prix;
+	  private Date dateDeReservation;
+	  private Date dateDeRentrer;
+	  private Date dateDeSortie;
+	  private String heureDeSortie;
+	  private String heureDeRentrer;
+	  private Integer nombresDeJours;
+	  private String etatDeReservation;
+	  private Reservation reservation;
+	  private List<Client> tabClientRes;
+	  private List<Voiture> tabVoitureRes;
+	  private List<Reservation> tabRes;
+	  private List<Reservation> tabHistRes;
+	  private String exceptionRes;
+	  private String exceptionDelRes;
+	  private String exceptionUpdateRes;
+	  
+	  public void addReservation() throws Exception{
+		  exceptionDelRes=null;
+		  exceptionRes=null;
+		  exceptionUpdateRes=null;
+		  try {
+			  Reservation r=new Reservation(new Date(), dateDeRentrer, dateDeSortie, heureDeSortie, heureDeRentrer, etatDeReservation);
+			  serviceReservation.addreservation(r, idClient, idVoiture);
+		} catch (Exception e) {
+			exceptionRes=e.getMessage();
+		}
+		  getAllReservation();
+		  getAllClient();
+		  getAllVoiture();
+	  }
+	  
+	  @PostConstruct
+	  public void getAllReservation(){
+		  tabRes=serviceReservation.getRes();
+	  }
+	  
+	  @PostConstruct
+	  public void getAllClient() throws Exception{
+		  tabClientRes=serviceClient.getClients();
+	  }
+	  @PostConstruct
+	  public void getAllVoiture(){
+		  tabVoitureRes=null;
+		  tabVoitureRes=serviceVoiture.getVoitures();
+	  }
+	  
+	  public void deleteRes() {
+		  exceptionDelRes=null;
+		  exceptionRes=null;
+		  exceptionUpdateRes=null;
+		 try {
+			  serviceReservation.deleteReservation(idReservation);
+		} catch (Exception e) {
+			System.out.println("Echec>--------------------------");
+			exceptionDelRes=e.getMessage();
+		}
+		  getAllReservation();
+	  }
+	  @PostConstruct
+	  public void histRes(){
+		  tabHistRes=serviceReservation.getHistoriqueReservation();
+	  }
+	  
+	  public void getResById(){
+		  reservation=serviceReservation.getResById(idReservation);
+	  }
+	  
+	  public void updateRes() throws Exception{
+		  exceptionDelRes=null;
+		  exceptionRes=null;
+		  exceptionUpdateRes=null;
+		  try {
+			  serviceReservation.updateReservation(reservation);
+		} catch (Exception e) {
+			exceptionUpdateRes=e.getMessage();
+		}
+		  getAllReservation();
+		  getAllClient();
+		  getAllVoiture();
+	  }
+	  //Constructor
+	  
+	  public ReservationBean() {
+			// TODO Auto-generated constructor stub
+		}
+	  
+	  //Get and Set
+	  
+	
+	public IReservationService getServiceReservation() {
+		return serviceReservation;
+	}
+	public void setServiceReservation(IReservationService serviceReservation) {
+		this.serviceReservation = serviceReservation;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public Date getDateDeReservation() {
+		return dateDeReservation;
+	}
+	public void setDateDeReservation(Date dateDeReservation) {
+		this.dateDeReservation = dateDeReservation;
+	}
+	public Date getDateDeRentrer() {
+		return dateDeRentrer;
+	}
+	public void setDateDeRentrer(Date dateDeRentrer) {
+		this.dateDeRentrer = dateDeRentrer;
+	}
+	public Date getDateDeSortie() {
+		return dateDeSortie;
+	}
+	public void setDateDeSortie(Date dateDeSortie) {
+		this.dateDeSortie = dateDeSortie;
+	}
+	public String getHeureDeSortie() {
+		return heureDeSortie;
+	}
+	public void setHeureDeSortie(String heureDeSortie) {
+		this.heureDeSortie = heureDeSortie;
+	}
+	public String getHeureDeRentrer() {
+		return heureDeRentrer;
+	}
+	public void setHeureDeRentrer(String heureDeRentrer) {
+		this.heureDeRentrer = heureDeRentrer;
+	}
+	public Integer getNombresDeJours() {
+		return nombresDeJours;
+	}
+	public void setNombresDeJours(Integer nombresDeJours) {
+		this.nombresDeJours = nombresDeJours;
+	}
+	public String getEtatDeReservation() {
+		return etatDeReservation;
+	}
+	public void setEtatDeReservation(String etatDeReservation) {
+		this.etatDeReservation = etatDeReservation;
+	}
+	public Long getIdClient() {
+		return idClient;
+	}
+	public void setIdClient(Long idClient) {
+		this.idClient = idClient;
+	}
+	public Long getIdVoiture() {
+		return idVoiture;
+	}
+	public void setIdVoiture(Long idVoiture) {
+		this.idVoiture = idVoiture;
+	}
+	public List<Client> getTabClientRes() {
+		return tabClientRes;
+	}
+	public void setTabClientRes(List<Client> tabClientRes) {
+		this.tabClientRes = tabClientRes;
+	}
+	public List<Voiture> getTabVoitureRes() {
+		return tabVoitureRes;
+	}
+	public void setTabVoitureRes(List<Voiture> tabVoitureRes) {
+		this.tabVoitureRes = tabVoitureRes;
+	}
+
+	public List<Reservation> getTabRes() {
+		return tabRes;
+	}
+
+	public void setTabRes(List<Reservation> tabRes) {
+		this.tabRes = tabRes;
+	}
+
+	public Long getIdReservation() {
+		return idReservation;
+	}
+
+	public void setIdReservation(Long idReservation) {
+		this.idReservation = idReservation;
+	}
+
+	public List<Reservation> getTabHistRes() {
+		return tabHistRes;
+	}
+
+	public void setTabHistRes(List<Reservation> tabHistRes) {
+		this.tabHistRes = tabHistRes;
+	}
+
+	public Reservation getReservation() {
+		return reservation;
+	}
+
+	public void setReservation(Reservation reservation) {
+		this.reservation = reservation;
+	}
+
+	public String getExceptionRes() {
+		return exceptionRes;
+	}
+
+	public void setExceptionRes(String exceptionRes) {
+		this.exceptionRes = exceptionRes;
+	}
+
+	public String getExceptionDelRes() {
+		return exceptionDelRes;
+	}
+
+	public void setExceptionDelRes(String exceptionDelRes) {
+		this.exceptionDelRes = exceptionDelRes;
+	}
+
+	public String getExceptionUpdateRes() {
+		return exceptionUpdateRes;
+	}
+
+	public void setExceptionUpdateRes(String exceptionUpdateRes) {
+		this.exceptionUpdateRes = exceptionUpdateRes;
+	}
+	  
+	
+	 
+	  
+}
--- BookingVoiture/src/main/java/com/adaming/bookingvoiture/dao/bean/VoitureBean.java
+++ BookingVoiture/src/main/java/com/adaming/bookingvoiture/dao/bean/VoitureBean.java
@@ -1,15 +1,313 @@
 package com.adaming.bookingvoiture.dao.bean;
 
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.RequestScoped;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.adaming.bookingvoiture.entities.Voiture;
+import com.adaming.bookingvoiture.exceptions.ExceptionDeleteVoiture;
+import com.adaming.bookingvoiture.exceptions.ExceptionDispoVoiture;
+import com.adaming.bookingvoiture.exceptions.ExceptionKilometrage;
 import com.adaming.bookingvoiture.service.voiture.IVoitureService;
 
-/*@Component("VoitureBean")
+@Component("VoitureBean")
 @RequestScoped
 public class VoitureBean {
-@Autowired
+	
+	Logger log=Logger.getLogger("VoitureBean");
+	
+	@Autowired
 	private IVoitureService serviceVoiture;
-}*/
+
+	//Attributs
+	
+	private Long idVoiture=0L;
+	private Date dateEntree;
+	private Date dateSortie;
+	private String model;
+	private String immatricule;
+	private Double kilometrage;
+	private double prixLocationJournee;
+	private String typeVoiture;
+	private String typeCarburant;
+	private String etatVoiture;
+	private Voiture voiture;
+	private List<Voiture> tabVoitures;
+	private List<Voiture> tabVoitureDispoToday;
+	private List<Voiture> tabVoituredispoPeriode;
+	private List<Voiture> tabRetourVoiture;
+	private String exceptionDelVoiture;
+	private String exceptionDispoToday;
+	private String exceptionDispoPeriode;
+	private String exceptionRetour;
+	private String exceptionkmEntretient;
+	private double kmEntretienRestant;
+	private String exceptionkmAllEntretien=null;
+	
+	public void addVoiture(){
+		Voiture v =new Voiture(model, immatricule, kilometrage, prixLocationJournee, typeVoiture, typeCarburant, etatVoiture);
+		serviceVoiture.addVoiture(v);
+		getAllVoiture();
+	}
+	
+	@PostConstruct
+	public void getAllVoiture(){
+		exceptionkmEntretient=null;
+		tabVoitures=serviceVoiture.getVoitures();
+	}
+	
+	public void deleteVoiture() throws ExceptionDeleteVoiture{
+		try {
+			serviceVoiture.deleteVoiture(idVoiture);
+		} catch (Exception e) {
+			exceptionDelVoiture=e.getMessage();
+		}
+		getAllVoiture();
+	}
+	
+	@PostConstruct
+	public void voitureDispoToday() throws ExceptionDispoVoiture, ExceptionKilometrage{
+		try {
+			tabVoitureDispoToday=serviceVoiture.voitureDispoToday();
+		} catch (Exception e) {
+			exceptionDispoToday=e.getMessage();
+		}
+		
+	}
+	
+	public void voitureDispoPeriode() throws ExceptionDeleteVoiture, ExceptionDispoVoiture{
+		tabVoituredispoPeriode=null;
+		exceptionDispoPeriode=null;
+		try {
+			tabVoituredispoPeriode=serviceVoiture.voitureDispoPeriode(dateEntree, dateSortie);
+		} catch (Exception e) {
+			exceptionDispoPeriode=e.getMessage();
+		}
+		getAllVoiture();
+	}
+	
+	@PostConstruct
+	public void alertAllEntretien(){
+		exceptionkmAllEntretien=null;
+		try {
+			exceptionkmAllEntretien=null;
+			serviceVoiture.alertAllEntretien();
+			exceptionkmAllEntretien=null;
+		} catch (Exception e) {
+			exceptionkmAllEntretien=e.getMessage();
+		}
+	}
+
+	public void getVoitureById(){
+		voiture=serviceVoiture.getVoitureById(idVoiture);
+	}
+	public void updateVoiture(){
+		serviceVoiture.updateVoiture(voiture);
+		getAllVoiture();
+	}
+	
+	public void alertEntretien() throws ExceptionKilometrage{
+			kmEntretienRestant= serviceVoiture.alertEntretien(idVoiture);
+	}
+	
+	@PostConstruct
+	public void retourVoiture() throws ParseException{
+		try {
+			tabRetourVoiture=serviceVoiture.retourVoiture();
+		} catch (Exception e) {
+			exceptionRetour=e.getMessage();
+		}
+	}
+	
+	//Constructor
+	
+	public VoitureBean() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//Get and Set
+	
+	
+	public IVoitureService getServiceVoiture() {
+		return serviceVoiture;
+	}
+	public void setServiceVoiture(IVoitureService serviceVoiture) {
+		this.serviceVoiture = serviceVoiture;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getImmatricule() {
+		return immatricule;
+	}
+	public void setImmatricule(String immatricule) {
+		this.immatricule = immatricule;
+	}
+	public Double getKilometrage() {
+		return kilometrage;
+	}
+	public void setKilometrage(Double kilometrage) {
+		this.kilometrage = kilometrage;
+	}
+	public double getPrixLocationJournee() {
+		return prixLocationJournee;
+	}
+	public void setPrixLocationJournee(double prixLocationJournee) {
+		this.prixLocationJournee = prixLocationJournee;
+	}
+	public String getTypeVoiture() {
+		return typeVoiture;
+	}
+	public void setTypeVoiture(String typeVoiture) {
+		this.typeVoiture = typeVoiture;
+	}
+	public String getTypeCarburant() {
+		return typeCarburant;
+	}
+	public void setTypeCarburant(String typeCarburant) {
+		this.typeCarburant = typeCarburant;
+	}
+	public String getEtatVoiture() {
+		return etatVoiture;
+	}
+	public void setEtatVoiture(String etatVoiture) {
+		this.etatVoiture = etatVoiture;
+	}
+
+	public List<Voiture> getTabVoitures() {
+		return tabVoitures;
+	}
+
+	public void setTabVoitures(List<Voiture> tabVoitures) {
+		this.tabVoitures = tabVoitures;
+	}
+
+	public Long getIdVoiture() {
+		return idVoiture;
+	}
+
+	public void setIdVoiture(Long idVoiture) {
+		this.idVoiture = idVoiture;
+	}
+
+	public List<Voiture> getTabVoitureDispoToday() {
+		return tabVoitureDispoToday;
+	}
+
+	public void setTabVoitureDispoToday(List<Voiture> tabVoitureDispoToday) {
+		this.tabVoitureDispoToday = tabVoitureDispoToday;
+	}
+
+	public Date getDateEntree() {
+		return dateEntree;
+	}
+
+	public void setDateEntree(Date dateEntree) {
+		this.dateEntree = dateEntree;
+	}
+
+	public Date getDateSortie() {
+		return dateSortie;
+	}
+
+	public void setDateSortie(Date dateSortie) {
+		this.dateSortie = dateSortie;
+	}
+
+	public List<Voiture> getTabVoituredispoPeriode() {
+		return tabVoituredispoPeriode;
+	}
+
+	public void setTabVoituredispoPeriode(List<Voiture> tabVoituredispoPeriode) {
+		this.tabVoituredispoPeriode = tabVoituredispoPeriode;
+	}
+
+	public Voiture getVoiture() {
+		return voiture;
+	}
+
+	public void setVoiture(Voiture voiture) {
+		this.voiture = voiture;
+	}
+
+	
+	public List<Voiture> getTabRetourVoiture() {
+		return tabRetourVoiture;
+	}
+
+	public void setTabRetourVoiture(List<Voiture> tabRetourVoiture) {
+		this.tabRetourVoiture = tabRetourVoiture;
+	}
+
+	public String getExceptionDelVoiture() {
+		return exceptionDelVoiture;
+	}
+
+	public void setExceptionDelVoiture(String exceptionDelVoiture) {
+		this.exceptionDelVoiture = exceptionDelVoiture;
+	}
+
+	public String getExceptionDispoToday() {
+		return exceptionDispoToday;
+	}
+
+	public void setExceptionDispoToday(String exceptionDispoToday) {
+		this.exceptionDispoToday = exceptionDispoToday;
+	}
+
+	public String getExceptionDispoPeriode() {
+		return exceptionDispoPeriode;
+	}
+
+	public void setExceptionDispoPeriode(String exceptionDispoPeriode) {
+		this.exceptionDispoPeriode = exceptionDispoPeriode;
+	}
+
+	public String getExceptionRetour() {
+		return exceptionRetour;
+	}
+
+	public void setExceptionRetour(String exceptionRetour) {
+		this.exceptionRetour = exceptionRetour;
+	}
+
+	public String getExceptionkmEntretient() {
+		return exceptionkmEntretient;
+	}
+
+	public void setExceptionkmEntretient(String exceptionkmEntretient) {
+		this.exceptionkmEntretient = exceptionkmEntretient;
+	}
+
+	public double getKmEntretienRestant() {
+		return kmEntretienRestant;
+	}
+
+	public void setKmEntretienRestant(double kmEntretienRestant) {
+		this.kmEntretienRestant = kmEntretienRestant;
+	}
+
+	public String getExceptionkmAllEntretien() {
+		return exceptionkmAllEntretien;
+	}
+
+	public void setExceptionkmAllEntretien(String exceptionkmAllEntretien) {
+		this.exceptionkmAllEntretien = exceptionkmAllEntretien;
+	}
+
+	
+
+	
+}
